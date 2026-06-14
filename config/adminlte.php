48d81395cc8793b3066b6852a02a4f655ac28708
rafildzuardi-blip <rafildzuardi@gmail.com>
2026-06-15 05:19:55 +0700

    tambahan

--- config/adminlte.php
+++ config/adminlte.php
@@ -459,12 +459,12 @@ return [
             ],
         ],
         'Sweetalert2' => [
-            'active' => false,
+            'active' => true, // SUDAH AKTIF SEKARANG
             'files' => [
                 [
                     'type' => 'js',
                     'asset' => false,
-                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@8',
+                    'location' => '//cdn.jsdelivr.net/npm/sweetalert2@11', // VERSI BARU
                 ],
             ],
         ],
